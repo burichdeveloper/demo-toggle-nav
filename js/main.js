bae603df61548745f9622d21ea95eee727db3d48
Rod Burich <burich.developer@gmail.com>
2021-03-01 18:45:44 -0700

    completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-// js code to trigger the button goes here
+// Step 1: query the document to select the element the user will click to show and hide the nav
+
+window.addEventListener('load', function(e){
+    const button = document.querySelector('.hamburguer-btn')
+
+    button.addEventListener('click', function(e){
+        const nav = document.querySelector('nav')
+        nav.classList.toggle('show-nav')
+    })
+})
